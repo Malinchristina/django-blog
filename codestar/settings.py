6e1c6a887811c0bcdf50242b58c1f1f5c6aeb32b
Malinchristina <malin_christina@yahoo.se>
2024-04-30 14:37:21 +0200

    Add addresses to allowed_hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wyjqt@at&!lgs^^v-dpu+@@bdn5pgq7$347&ag=e(p7pfd8&=3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','.herokuapp.com']
 
 # Application definition
 
