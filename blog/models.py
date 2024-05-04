b16556dfd82dd2d7157f4912763c54c3a779c56f
Malinchristina <malin_christina@yahoo.se>
2024-05-04 11:37:38 +0200

    Add meta and method to comment model

--- blog/models.py
+++ blog/models.py
@@ -16,7 +16,7 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
     class Meta:
-        ordering = ["-created_on"]
+        ordering = ["created_on"]
     def __str__(self):
         return f"{self.title} | written by {self.author}"
 
@@ -30,5 +30,9 @@ class Comment(models.Model):
     )
     body = models.TextField()
     approved = models.BooleanField(default=False)
-    created_on = models.DateTimeField(auto_now_add=True)    
+    created_on = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ["created_on"]
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
 
