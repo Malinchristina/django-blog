01910aceaddbffd4756101c5d436eee65c4b53a8
Malinchristina <malin_christina@yahoo.se>
2024-05-09 09:27:58 +0200

    Correct settings from challenge

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = ('django-insecure-wyjqt@at&!lgs^^v-dpu+@@bdn5pgq7$347&ag=e(p7pfd8&=3')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1','.herokuapp.com']
 
